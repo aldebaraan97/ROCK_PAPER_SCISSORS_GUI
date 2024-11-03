2aa61bd9d63f5707f2e0e446f78313f0693ff751
Aldebaraan <aldecanedo97@gmail.com>
2024-11-03 01:35:52 -0400

    Forth commit

--- src/main/java/com/example/rock_paper_scissors/GamePane/GamePane.java
+++ src/main/java/com/example/rock_paper_scissors/GamePane/GamePane.java
@@ -1,13 +1,16 @@
 package com.example.rock_paper_scissors.GamePane;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class GamePane extends Pane {
+    private TextField userSelection = new TextField();
+    private TextField randomSelection = new TextField();
     private Button playBt = new Button("PLAY!");
 
-    GridPane griPane = new GridPane();
-    gridPane.add(new Label("Enter your election"),);
+    // Create GUI
+
 
 }
--- src/main/java/com/example/rock_paper_scissors/HelloApplication.java
+++ src/main/java/com/example/rock_paper_scissors/HelloApplication.java
@@ -12,30 +12,34 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-
-    private TextField userInput = new TextField();
-    private TextField randomElection;
-
+    private TextField userSelection = new TextField();
+    private TextField randomSelection = new TextField();
+    private Button playBt = new Button("PLAY!");
 
     @Override
     public void start(Stage stage) {
+
         GamePane gamePane = new GamePane();
 
-        // Create GUI
-        GridPane gridPane = new GridPane();
-        gridPane.setVgap(10);
-        gridPane.setHgap(10);
+        // Introduction label
         String introduction = """
                 Program to play ROCK, PAPER, SCISSORS game against the computer.
                 Enter only 'ROCK', 'PAPER' or 'SCISSORS' (case insensitive).
                 Any other input will assign the user a random choice.""";
 
-        gridPane.add(new Label(introduction), 0, 0);
+        // Create UI for game
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.add(new Label("Type your selection:"), 0, 0);
+        gridPane.add(userSelection, 1, 0);
+        gridPane.add(new Label("Random selection: "), 0, 1);
+        gridPane.add(randomSelection, 1,1);
 
         // Place introduction and text fields in border pane
         BorderPane pane = new BorderPane();
         pane.setTop(new Label(introduction));
-        pane.setBottom(gamePane);
+        pane.setCenter(gridPane);
 
         // Create a scene and place it in the stage
         Scene scene = new Scene(pane, 400, 250);
